db696a5c9bd2c10373f22baf3c843b64f21f93a3
amartinezfer@google.com <ubuntu@ip-172-31-3-212.ec2.internal>
2023-01-04 07:37:55 +0000

    add region

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -3,4 +3,6 @@
 set -x
 du -hs * | sort -h
 export AWS_REGION=us-east-1
+export AWS_DEFAULT_REGION=us-east-1
+
 sam deploy template.yaml --config-env ${ENVIRONMENT}  --region us-east-1 --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --stack-name $1
