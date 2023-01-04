05a0b19ed68fe9170b5464c14305a7acd160423a
amartinezfer@google.com <ubuntu@ip-172-31-3-212.ec2.internal>
2023-01-04 06:26:51 +0000

    change virtual

--- pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
+++ pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 set -x
-python3.7 -m venv todo-list-aws
+
+pip3 install virtualenv
+
+virtualenv todo-list-aws
 source todo-list-aws/bin/activate
 python -m pip install --upgrade pip
 python -m pip install awscli
 python -m pip install aws-sam-cli
 # For integration testing
 python -m pip install pytest
-pwd
