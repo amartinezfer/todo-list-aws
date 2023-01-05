ebb3b0aa49eb5cfcefb83b7f3fe27fb7308481a2
Ubuntu <ubuntu@ip-172-31-8-76.ec2.internal>
2023-01-05 07:07:29 +0000

    modify virtual

--- pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
+++ pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
@@ -2,10 +2,9 @@
 
 set -x
 
-pip3 install virtualenv
-
-virtualenv todo-list-aws
+python3.7 -m venv todo-list-aws
 source todo-list-aws/bin/activate
+
 python -m pip install --upgrade pip
 python -m pip install awscli
 python -m pip install aws-sam-cli
