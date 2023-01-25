8812688d2bb10210c3d949a64155b974e82e73f9
amartinezfer <amartinezfer@gmail.com>
2023-01-25 20:30:43 +0100

    add feature

---
+++ src/gettrans.py
@@ -0,0 +1,25 @@
+import json
+import decimalencoder
+import todoList
+
+
+def gettrans(event, context):
+    # create a response
+
+    language = event['pathParameters']['language']
+    id = event['pathParameters']['id']
+    lan = event['pathParameters']['language']
+    item = todoList.get_item_translate(id,lan)
+    
+    if item:
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(item,
+                               cls=decimalencoder.DecimalEncoder)
+        }
+    else:
+        response = {
+            "statusCode": 404,
+            "body": ""
+        }
+    return response
